9e2774855eb17caf9e2bd620dba73ff6721322df
mohanpemmadi <connectpypi@gmail.com>
2022-03-05 07:54:11 +0530

    modules

---
+++ 17_modules.py
@@ -0,0 +1,37 @@
+""" Module - Module is a python file that can be imported inside another file """
+
+# 1
+
+# import calc
+#
+# print(calc.name)
+# print(calc.sub(10,5))
+# print(calc.add(10,5))
+# print(calc.mul(10,5))
+# print(calc.div(10,5))
+
+# 2
+
+# import calc as cl
+# print(cl.name)
+# print(cl.add(10,20))
+
+# 3
+
+# from calc import add,div
+# print(add(10,2))
+# print(div(20,10))
+# print(sub(10,2))
+
+# 4
+
+from calc import *
+
+print(name)
+print(sub(10,5))
+print(mul(10,20))
+
+
+
+
+
